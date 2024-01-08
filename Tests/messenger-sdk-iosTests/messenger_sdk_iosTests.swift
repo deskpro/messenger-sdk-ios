a6ecdd9b0d6ca06e6533f2781b69f003173f2f6f
QSD_faris <info@qsdbih.com>
2024-01-08 09:20:29 +0100

    fix test method

--- Tests/messenger-sdk-iosTests/messenger_sdk_iosTests.swift
+++ Tests/messenger-sdk-iosTests/messenger_sdk_iosTests.swift
@@ -6,6 +6,6 @@ final class messenger_sdk_iosTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(messenger_sdk_ios().text, "Hello, World?")
+        XCTAssertEqual(messenger_sdk_ios().text, "Hello, World!")
     }
 }
