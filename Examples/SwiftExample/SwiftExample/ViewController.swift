ea55ccb011334152c8786e00e6a63fa5be1e836d
QSD_faris <info@qsdbih.com>
2024-01-08 15:12:36 +0100

    improve swift example

--- Examples/SwiftExample/SwiftExample/ViewController.swift
+++ Examples/SwiftExample/SwiftExample/ViewController.swift
@@ -14,17 +14,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var testLbl: UILabel!
     @IBOutlet weak var openBtn: UIButton!
     
-    let deskproUrlString = "https://www.deskpro.com"
-    let messengerUrlString = "http://178.62.74.147:3001/d/B0ED0B34-8507-4248-8738-0B0D80A6F7E9/"
     let messengerConfig = MessengerConfig(appUrl: "http://178.62.74.147:3001/d/", appId: "B0ED0B34-8507-4248-8738-0B0D80A6F7E9")
     var messenger: DeskPro?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        messenger = DeskPro(messengerConfig: messengerConfig)
-        
-        messenger?.present().show()
+        messenger = DeskPro(messengerConfig: messengerConfig, containingViewController: self)
     }
 
     @IBAction func testTapped(_ sender: Any) {
@@ -34,17 +30,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func openTapped(_ sender: Any) {
-        openWebView(urlString: messengerUrlString)
-    }
-    
-    private func openWebView(urlString: String) {
-        let customWebView = CustomWebView()
-
-        if let url = URL(string: urlString) {
-            customWebView.configureURL(url)
-        }
-
-        present(customWebView, animated: true)
+        messenger?.present().show()
     }
 }
 
