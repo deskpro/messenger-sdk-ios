a96fbb55842a7e0a6e9b57f0695187c8a03b030a
QSD_faris <info@qsdbih.com>
2024-01-14 23:53:11 +0100

    update example project based on new xcframework changes

--- Examples/SwiftExample/SwiftExample/ViewController.swift
+++ Examples/SwiftExample/SwiftExample/ViewController.swift
@@ -10,11 +10,13 @@ import DeskproFramework
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var openBtn: UIButton!
     @IBOutlet weak var testBtn: UIButton!
     @IBOutlet weak var testLbl: UILabel!
-    @IBOutlet weak var openBtn: UIButton!
     
-    let messengerConfig = MessengerConfig(appUrl: "http://178.62.74.147:3001/d/", appId: "B0ED0B34-8507-4248-8738-0B0D80A6F7E9")
+    //let messengerConfig = MessengerConfig(appUrl: "http://178.62.74.147:3001/d/", appId: "B0ED0B34-8507-4248-8738-0B0D80A6F7E9")
+    let messengerConfig = MessengerConfig(appUrl: "https://dev-pr-12670.earthly.deskprodemo.com/deskpro-messenger/deskpro/1/d/", appId: "")
+    
     var messenger: DeskPro?
     
     override func viewDidLoad() {
@@ -22,15 +24,13 @@ class ViewController: UIViewController {
         
         messenger = DeskPro(messengerConfig: messengerConfig, containingViewController: self)
     }
-
-    @IBAction func testTapped(_ sender: Any) {
-        if let messenger {
-            testLbl.text = messenger.test()
-        }
-    }
     
     @IBAction func openTapped(_ sender: Any) {
         messenger?.present().show()
+    }
+    
+    @IBAction func testTapped(_ sender: Any) {
+        testLbl.text = messenger?.test()
     }
 }
 
